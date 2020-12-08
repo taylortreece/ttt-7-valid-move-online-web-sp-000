a500bbfb75fe66602dad6a914cc1c2467cbe9cde
Taylor Treece <tayloratreece@gmail.com>
2020-12-08 23:20:18 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,8 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
   position_taken?(board, index)
+else 
+  return nil
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
