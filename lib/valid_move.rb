d469843efea011177ea061d36acec730f018aa8f
Taylor Treece <tayloratreece@gmail.com>
2020-12-08 23:43:56 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ def valid_move?(board, index)
   if  position_taken?(board, index)
     return true
   elsif index.between?(0, 8) == false
-    return false
+    return index.between?(0, 8)
   end
 end
 
