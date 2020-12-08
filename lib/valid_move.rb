984ff9252227d471a98129790699dd9970c1e34e
Taylor Treece <tayloratreece@gmail.com>
2020-12-08 23:29:44 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
     position_taken?(board, index)
-    if index.between?(0,8) === false
+    if index.between?(0,8) = false
       return false
     end
 end
